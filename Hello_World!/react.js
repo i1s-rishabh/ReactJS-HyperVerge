40c105ff72c40a3a1e9ee09d2248dd34e0552553
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-04-07 00:58:18 +0530

    Delete Hello_World! directory

--- Hello_World!/react.js
+++
@@ -1,8 +0,0 @@
-const domContainer = document.querySelector("#container");
-reactDOM.render(<h1>Hello, World!</h1>, domContainer) 
-
-// FIRST parameter of .render() method
-    // what is want to render in the first parameter i've to write.
-
-// SECOND parameter of .render() method
-    // where do i want ot render it.
