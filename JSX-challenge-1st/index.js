efdbd320f2a554b08c19d7771eecf2c5fa561d0d
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-04-08 12:27:47 +0530

    JSX-challange

---
+++ JSX-challenge-1st/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//const space = document.getElementById('root');
+
+ReactDOM.render(
+    <>
+        <h1>List of My 5 favorite Netflix Series</h1>
+        <p>Lost in Space is quite amazing</p>
+        <ol>
+            <li>Lost in space</li>
+            <li>Dark</li>
+            <li>Mr. Robot</li>
+            <li>Chilling Adventure of Sabrina</li>
+            <li>Titans</li>
+        </ol>
+    </>, document.getElementById('root')
+);
