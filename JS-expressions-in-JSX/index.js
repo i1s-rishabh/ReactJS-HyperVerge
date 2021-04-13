36cd7e27db1928ce483deb9c87b9641bad004fd9
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-04-13 15:32:55 +0530

    JSX-expressions

--- JS-expressions-in-JSX/index.js
+++ JS-expressions-in-JSX/index.js
@@ -8,6 +8,6 @@ ReactDOM.render(
     <p>This is my {name}</p> 
   // We need a pair of curly braces to access any js variable or expression
     <p>The sum of 2 & 3 is : {2+3}</p> 
-  // if we'll write the expression without curly braces, it'll render as 2 + 3 not 5
+  // if we'll write the expression without curly brackets, it'll render as 2 + 3 not 5
     </>,document.getElementById('root')
     );
