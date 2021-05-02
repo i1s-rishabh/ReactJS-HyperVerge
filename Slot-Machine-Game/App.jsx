e48fd461095380c3d51a401dc4e0dc4350823bf2
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-05-02 14:13:21 +0530

    Challenge #6

---
+++ Slot-Machine-Game/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SlotM from './SlotMachine';
+
+const App = () => {
+   return (
+      <> 
+         <h1 className="heading_style">
+            🎰 Welcome to <span style={{ fontWeight: "bold"}}> Slot Machine Game </span> 🎰
+         </h1>
+
+         <div className="slotmachine">
+            <SlotM x = "😀" y = "😀" z = "😀"/>
+            <SlotM x = "🐎" y = "🦌" z = "🐕"/> 
+            <SlotM x = "🐭" y = "😽" z = "🐭"/>
+            <SlotM x = "🍊" y = "🍊" z = "🍊"/>
+         </div> 
+      </>
+   );
+};
+
+export default App;
