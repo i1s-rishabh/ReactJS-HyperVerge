3ffe1683ba3a58edf8066053766f8a0e1ecde173
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-05-02 10:41:25 +0530

    Update Index.js (Adding app.jsx)

--- mini-netflix-app/index.js
+++ mini-netflix-app/index.js
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from './Cards';
-import Sdata from './Sdata';
+import App from "./App";
 import "./index.css";
 
-ReactDOM.render( // custom properties/attributes
-  
-  <> 
-  <h1 className="heading_style">List of top 6 Netflix Series in 2021</h1>
-       {Sdata.map((val) => {
-        return(
-        <Card 
-            imgsrc ={val.imgsrc}
-            title ={val.title}
-            sname ={val.sname}
-            link ={val.link}
-       />
-    )
-       })}
-   </>  , document.getElementById('root')
-);
+ReactDOM.render( <App /> , document.getElementById('root'));
