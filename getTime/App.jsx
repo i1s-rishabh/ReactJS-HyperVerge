0e498ceef805e92804091a18ab96cc7975658de7
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-05-05 15:02:39 +0530

    Digital-watch app

---
+++ getTime/App.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+const App = () => {
+   let newTime = new Date().toLocaleTimeString();
+   const [ctime, setCtime] = useState(newTime);
+
+   const UpdateTime = () => {
+      newTime = new Date().toLocaleTimeString();
+      setCtime(newTime);
+   }
+
+   return (
+      <>
+         <h1> {ctime} </h1>
+         <button onClick={UpdateTime}> get time </button>
+      </>
+   );
+};
+
+export default App;
