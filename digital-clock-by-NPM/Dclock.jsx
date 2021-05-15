5ea80e359705f2312549e37d041cdfb3cfba2d1a
Rishabh Sharma <43880492+i1s-rishabh@users.noreply.github.com>
2021-05-15 09:43:03 +0530

    Digital Clock By NPM (Node Package Manager)

---
+++ digital-clock-by-NPM/Dclock.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Clock from 'react-digital-clock';
+
+const Dclock = () => {
+    return(<Clock />)
+}
+
+export default Dclock;
---
+++ digital-clock-by-NPM/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import Dclock from "./Dclock"
+
+ReactDOM.render( <Dclock /> , document.getElementById('root'));
